0615bd382e5fedb81e4576c6e11f73b69d3451a9
ayanentity <ayanentity154@gmail.com>
2025-10-25 16:11:10 +0900

    color define

---
+++ lib/style/color.ts
@@ -0,0 +1,27 @@
+export const colors = {
+  primary: "#4F46E5",
+
+  label: {
+    primary: "#1F2937",
+    secondary: "#586070",
+    tertiary: "#B5BBC5",
+  },
+
+  background: {
+    primary: "#F8F9FA",
+    secondary: "#FFFFFF",
+    tertiary: "#E2E7EB",
+  },
+
+  line: {
+    primary: "#B0BACE",
+    secondary: "#DCE2ED",
+  },
+
+  accent: {
+    success: "#10B981",
+    error: "#FD635E",
+    warning: "#FFCA70",
+    info: "#61A0FF",
+  },
+} as const;
